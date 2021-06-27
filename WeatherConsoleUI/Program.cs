dc8c69fa1af9ee01a32e84d4b80902689ec4aa0d
Brian Fontana <brian@codefoxtrot.com>
2021-06-27 12:32:52 -0400

    Rewire DI to enable user secrets

--- WeatherConsoleUI/Program.cs
+++ WeatherConsoleUI/Program.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using System;
 using System.Threading.Tasks;
 using WeatherConsoleUI.Services;
@@ -10,26 +8,36 @@ namespace WeatherConsoleUI
 {
     class Program
     {
-        public static IConfigurationRoot Configuration;
+        public static IConfigurationRoot Configuration { get; set; }
 
         static async Task Main(string[] args)
         {
             string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            bool isDevelopment = string.IsNullOrEmpty(env) || env.ToLower() == "development";
 
             var builder = new ConfigurationBuilder()
                 .AddJsonFile($"appsettings.json", true, true)
                 .AddJsonFile($"appsettings.{env}.json", true, true)
                 .AddEnvironmentVariables();
 
+            if (isDevelopment)
+            {
+                builder.AddUserSecrets<Program>();
+            }
+
             Configuration = builder.Build();
 
-            using IHost host = CreateHostBuilder(args).Build();
-            using var scope = host.Services.CreateScope();
-            IServiceProvider services = scope.ServiceProvider;
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton<IConfiguration>(Configuration);
+            services.AddHttpClient<CurrentWeatherService>();
+            services.AddTransient<WeatherApp>();
+            services.BuildServiceProvider();
+
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
 
             try
             {
-                await services.GetRequiredService<WeatherApp>().Run();
+                await serviceProvider.GetService<WeatherApp>().Run();
             }
             catch (Exception e)
             {
@@ -39,17 +47,5 @@ namespace WeatherConsoleUI
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
-
-        static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureLogging(logger =>
-                {
-                    logger.ClearProviders();
-                })
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddHttpClient<CurrentWeatherService>();
-                    services.AddTransient<WeatherApp>();
-                });
     }
 }
