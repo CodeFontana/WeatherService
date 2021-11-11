a613bc871cfbe6831d2d72cead14d0b92d3d2f90
Brian Fontana <brian@codefoxtrot.com>
2021-11-11 17:12:14 -0500

    Update project to .NET 6.0, and take advantage of top-level statements

--- SimpleConsoleUI/Program.cs
+++ SimpleConsoleUI/Program.cs
@@ -2,42 +2,29 @@
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text.Json;
-using System.Threading.Tasks;
 
-namespace SimpleConsoleUI
+string apiKey = "<API key from openweathermap.org>";
+string city = "Center Moriches";
+string state = "New York";
+
+HttpRequestMessage request = new()
 {
-    class Program
+    Method = HttpMethod.Get,
+    RequestUri = new Uri($"https://api.openweathermap.org/data/2.5/weather?q={city},{state}&appid={apiKey}&units=imperial"),
+};
+
+using (HttpClient client = new())
+{
+    using (HttpResponseMessage response = await client.SendAsync(request))
     {
-        static async Task Main(string[] args)
+        if (response.IsSuccessStatusCode)
         {
-            string apiKey = "<API key from openweathermap.org>";
-            string city = "Center Moriches";
-            string state = "New York";
-
-            HttpRequestMessage request = new()
-            {
-                Method = HttpMethod.Get,
-                RequestUri = new Uri($"https://api.openweathermap.org/data/2.5/weather?q={city},{state}&appid={apiKey}&units=imperial"),
-            };
-
-            using (HttpClient client = new())
-            {
-                using (HttpResponseMessage response = await client.SendAsync(request))
-                {
-                    if (response.IsSuccessStatusCode)
-                    {
-                        object body = await response.Content.ReadFromJsonAsync<object>();
-                        Console.WriteLine(JsonSerializer.Serialize<object>(body, new JsonSerializerOptions { WriteIndented = true }));
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Request failed[{ response.StatusCode }]: { response.ReasonPhrase }");
-                    }
-                }
-            }
-
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
+            object body = await response.Content.ReadFromJsonAsync<object>();
+            Console.WriteLine(JsonSerializer.Serialize<object>(body, new JsonSerializerOptions { WriteIndented = true }));
+        }
+        else
+        {
+            Console.WriteLine($"Request failed[{ response.StatusCode }]: { response.ReasonPhrase }");
         }
     }
 }
