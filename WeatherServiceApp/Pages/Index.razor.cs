bc2bd807586d99e41dd070c3041ff895d24563e7
Brian Fontana <brian@codefoxtrot.com>
2023-03-11 18:19:53 -0500

    Fix filenames for partial classes

---
+++ WeatherServiceApp/Pages/Index.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using WeatherServiceApp.Interfaces;
+using WeatherServiceApp.Models;
+
+namespace WeatherServiceApp.Pages;
+
+public partial class Index
+{
+    [Inject] public ICurrentWeatherService ForecastService { get; set; }
+
+    private CurrentWeatherRequestModel _request = new();
+    private CurrentWeatherResultModel result;
+
+    private async Task HandleValidSubmit()
+    {
+        result = await ForecastService.GetCurrentWeatherAsync(_request.City, _request.State);
+    }
+}
